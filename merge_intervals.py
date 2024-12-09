2c37ac8fb990c394366f9e47acbf26a770dce1e8
Archana Suresh <archanasuresh@live.in>
2024-12-08 23:47:45 -0800

    adds merge intervals and stck buy

---
+++ merge_intervals.py
@@ -0,0 +1,9 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        merged_interval = []
+        for interval in intervals:
+            if not merged_interval or merged_interval[-1][1] < interval[0]:
+                merged_interval.append(interval)
+            else:
+                merged_interval[-1][1] = max(merged_interval[-1][1],interval[1])
+        return merged_interval
