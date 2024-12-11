d524fab35ef34f7b0fd0b56e7da7d513b52cef70
Archana Suresh <archanasuresh@live.in>
2024-12-11 09:12:26 -0800

    Leetcode solution blind75

--- merge_intervals.py
+++ merge_intervals.py
@@ -1,6 +1,9 @@
+from typing import List
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x: x[0])
         merged_interval = []
+        
         for interval in intervals:
             if not merged_interval or merged_interval[-1][1] < interval[0]:
                 merged_interval.append(interval)
